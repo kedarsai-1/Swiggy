a4fdb2131508a5ebfc7f7e4e0183fcc8c4b3be42
sai chaitanya <saichaitanya@sais-MacBook-Pro-2.local>
2025-07-03 21:25:19 +0530

    mistake

--- src/components/RestaurantCategory.jsx
+++ src/components/RestaurantCategory.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import ItemList from "./itemList.jsx";
+import ItemList from "./ItemList.jsx";
 const RestaurantCategory =({data,showItems,setshowIndex})=>{
     
 
